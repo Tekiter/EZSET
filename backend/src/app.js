48da2898f454514b0855b63e0a6df248b4c48ad9
Tekiter <tekiter8@gmail.com>
2019-11-07 15:01:51 +0900

    Configure ESLint and Prettier

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,33 @@
-import express from 'express';
-import path from 'path';
-import logger from 'morgan';
-import bodyParser from 'body-parser';
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import bodyParser from "body-parser";
 
 const app = express();
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
-
-app.use(logger('dev', {
-  skip: () => app.get('env') === 'test'
-}));
+app.use(
+  logger("dev", {
+    skip: () => app.get("env") === "test"
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../public')));
-
-
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // Error handler
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  res
-    .status(err.status || 500)
-    .render('error', {
-      message: err.message
-    });
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  res.status(err.status || 500).render("error", {
+    message: err.message
+  });
 });
 
 export default app;
