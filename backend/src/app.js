a789e21a8aa49907d943dfd3043f69b7f945c295
Tekiter <tekiter8@gmail.com>
2019-11-08 03:26:56 +0900

    Change settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,9 @@ const app = express()
 app.disable('x-powered-by')
 
 app.use(
-    logger('dev', {
-        skip: () => app.get('env') === 'test',
-    }),
+  logger('dev', {
+    skip: () => app.get('env') === 'test',
+  })
 )
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -17,15 +17,15 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
 })
 
 // Error handler
 app.use((err, req, res, next) => {
-    // eslint-disable-line no-unused-vars
-    res.status(err.status || 500).json({ message: err.message })
+  // eslint-disable-line no-unused-vars
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 export default app
