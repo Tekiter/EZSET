0f20292923dc261e8e56e7b439468c1526464d52
Tekiter <tekiter8@gmail.com>
2019-11-10 01:12:26 +0900

    Add database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,16 @@
+/* eslint-disable no-console */
 import app from './app'
+import database from './utils/database'
 
 const { PORT = 8080 } = process.env
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) // eslint-disable-line no-console
+
+database
+    .initialize()
+    .then(() => {
+        console.log('Successfully connected to database')
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    })
+    .catch(error => {
+        console.log('Failed to connect database')
+        console.error(error)
+    })
