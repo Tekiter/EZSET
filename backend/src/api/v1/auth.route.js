7d54d06e3d21f760fef4563afa8cef685a1656a4
Tekiter <tekiter8@gmail.com>
2019-11-10 04:19:01 +0900

    Add parameter validation

--- backend/src/api/v1/auth.route.js
+++ backend/src/api/v1/auth.route.js
@@ -1,46 +1,77 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 
 import auth from '../../utils/auth'
-import { databaseError, unexpectedError } from '../../utils/api'
+import { databaseError, unexpectedError, validateParams } from '../../utils/api'
 import User from '../../models/User'
 
 const router = Router()
 
-router.route('/login').post(async (req, res) => {
-    try {
-        const user = await User.findOne()
-            .where('username')
-            .equals(req.body.username)
+router
+    .route('/login')
+    .post(
+        [
+            body('username').isString(),
+            body('password').isString(),
+            validateParams,
+        ],
+        async (req, res) => {
+            try {
+                const user = await User.findOne()
+                    .where('username')
+                    .equals(req.body.username)
 
-        if (user && user.checkPassword(req.body.password)) {
-            const accessToken = await auth.createAccessToken(user)
+                if (user && user.checkPassword(req.body.password)) {
+                    const accessToken = await auth.createAccessToken(user)
 
-            res.status(200).json({
-                accessToken,
-            })
-        } else {
-            res.status(403).json({ message: 'login failed' })
+                    res.status(200).json({
+                        accessToken,
+                    })
+                } else {
+                    res.status(403).json({ message: '로그인 실패' })
+                }
+            } catch (error) {
+                databaseError(res, error)
+            }
         }
-    } catch (error) {
-        databaseError(res, error)
-    }
-})
+    )
 
-router.route('/register').post(async (req, res) => {
-    try {
-        const user = new User({
-            username: req.body.username,
-            password: req.body.password,
-            info: {
-                realname: req.body.realname,
-            },
-        })
-        await user.save()
+router
+    .route('/register')
+    .post(
+        [
+            body('username').isString(),
+            body('password').isString(),
+            body('realname').isString(),
+            validateParams,
+        ],
+        async (req, res) => {
+            try {
+                const exists = await User.count()
+                    .where('username')
+                    .equals(req.body.username)
 
-        res.status(201).json({ message: 'success' })
-    } catch (error) {
-        unexpectedError(res, error)
-    }
-})
+                if (exists) {
+                    res.status(409).json({
+                        message: '이미 사용중인 아이디입니다.',
+                    })
+                    return
+                }
+
+                const user = new User({
+                    username: req.body.username,
+                    password: req.body.password,
+                    info: {
+                        realname: req.body.realname,
+                    },
+                })
+                await user.save()
+
+                res.status(201).json({ message: 'success' })
+            } catch (error) {
+                unexpectedError(res, error)
+            }
+        }
+    )
 
 export default router
--- backend/src/utils/api.js
+++ backend/src/utils/api.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+const { validationResult } = require('express-validator')
+
 export function databaseError(res, error) {
     const errfunc = err => {
         console.log(err)
@@ -22,5 +24,18 @@ export function unexpectedError(res, error) {
         errfunc(error)
     } else {
         return errfunc
+    }
+}
+
+export function validateParams(req, res, next) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() })
+        return false
+    } else {
+        if (next) {
+            next()
+        }
+        return true
     }
 }
