2e186ab08c35175c6b764f6e0d9c7b0b8ffa7749
Tekiter <tekiter8@gmail.com>
2019-11-10 18:52:07 +0900

    Add auth middleware

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -37,6 +37,23 @@ const auth = {
             })
         })
     },
+    async loginRequired(req, res, next) {
+        if (req.headers && req.headers.authorization) {
+            let tokenbase = req.headers.authorization.split(' ')
+            if (tokenbase[0] === 'Bearer') {
+                try {
+                    const user = await auth.checkToken(tokenbase[1])
+                    req.user = user
+                    next()
+                    return
+                } catch (error) {
+                    res.status(401).json({ message: '로그인이 필요합니다.' })
+                    return
+                }
+            }
+        }
+        res.status(401).json({ message: '로그인이 필요합니다.' })
+    },
 }
 
 export default auth
