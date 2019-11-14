4b4e9a287ec805ffc536aa400cda7c43c041f7b5
Tekiter <tekiter8@gmail.com>
2019-11-15 03:42:52 +0900

    Add asyncRoute wrap function

--- backend/src/api/v1/auth.route.js
+++ backend/src/api/v1/auth.route.js
@@ -2,77 +2,74 @@ import { Router } from 'express'
 import { body } from 'express-validator'
 
 import auth from '../../utils/auth'
-import { databaseError, unexpectedError, validateParams } from '../../utils/api'
+import {
+    databaseError,
+    unexpectedError,
+    validateParams,
+    asyncRoute,
+} from '../../utils/api'
 import User from '../../models/User'
 
 const router = Router()
 router.loginNotRequired = true
 
-router
-    .route('/login')
-    .post(
-        [
-            body('username').isString(),
-            body('password').isString(),
-            validateParams,
-        ],
-        async (req, res) => {
-            try {
-                const user = await User.findOne()
-                    .where('username')
-                    .equals(req.body.username)
+router.route('/login').post(
+    [body('username').isString(), body('password').isString(), validateParams],
+    asyncRoute(async (req, res) => {
+        try {
+            const user = await User.findOne()
+                .where('username')
+                .equals(req.body.username)
 
-                if (user && user.checkPassword(req.body.password)) {
-                    const accessToken = await auth.createAccessToken(user)
+            if (user && user.checkPassword(req.body.password)) {
+                const accessToken = await auth.createAccessToken(user)
 
-                    res.status(200).json({
-                        accessToken,
-                    })
-                } else {
-                    res.status(403).json({ message: '로그인 실패' })
-                }
-            } catch (error) {
-                databaseError(res, error)
-            }
-        }
-    )
-
-router
-    .route('/register')
-    .post(
-        [
-            body('username').isString(),
-            body('password').isString(),
-            body('realname').isString(),
-            validateParams,
-        ],
-        async (req, res) => {
-            try {
-                const exists = await User.count()
-                    .where('username')
-                    .equals(req.body.username)
-
-                if (exists) {
-                    res.status(409).json({
-                        message: '이미 사용중인 아이디입니다.',
-                    })
-                    return
-                }
-
-                const user = new User({
-                    username: req.body.username,
-                    password: req.body.password,
-                    info: {
-                        realname: req.body.realname,
-                    },
+                res.status(200).json({
+                    accessToken,
                 })
-                await user.save()
-
-                res.status(201).json({ message: 'success' })
-            } catch (error) {
-                unexpectedError(res, error)
+            } else {
+                res.status(403).json({ message: '로그인 실패' })
             }
+        } catch (error) {
+            databaseError(res, error)
         }
-    )
+    })
+)
+
+router.route('/register').post(
+    [
+        body('username').isString(),
+        body('password').isString(),
+        body('realname').isString(),
+        validateParams,
+    ],
+    asyncRoute(async (req, res) => {
+        try {
+            const exists = await User.count()
+                .where('username')
+                .equals(req.body.username)
+
+            if (exists) {
+                res.status(409).json({
+                    message: '이미 사용중인 아이디입니다.',
+                })
+                return
+            }
+
+            const user = new User({
+                username: req.body.username,
+                password: req.body.password,
+                info: {
+                    realname: req.body.realname,
+                },
+            })
+            await user.save()
+
+            res.status(201).json({ message: 'success' })
+        } catch (error) {
+            unexpectedError(res, error)
+        }
+    })
+)
 
 export default router
