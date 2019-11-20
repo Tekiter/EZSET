4dcacd5953f28fded61dfa7d6ae712a7c5d0bc04
EZYOON <fullmoon5110@gmail.com>
2019-11-20 22:57:55 +0900

    Add validation

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -7,33 +7,43 @@
         <v-content>
             <v-card :loading="isloading">
                 <v-stepper v-model="curpage" vertical>
-                    <v-stepper-step :complete="curpage > 1" step="1">
+                    <v-stepper-step
+                        :complete="curpage > 1"
+                        :rules="[() => !errorRequiredStep]"
+                        step="1"
+                    >
                         필수정보입력
                     </v-stepper-step>
 
                     <v-stepper-content step="1">
                         <v-text-field
                             v-model="form.realname"
-                            :counter="10"
                             label="name"
+                            @input="removeError('realname')"
+                            :error-messages="errors.realname"
                             required
                         ></v-text-field>
                         <v-text-field
-                            v-model="form.id"
-                            :counter="10"
+                            v-model="form.username"
                             label="ID"
+                            @input="removeError('username')"
+                            :error-messages="errors.username"
                             required
                         ></v-text-field>
                         <v-text-field
                             v-model="form.password"
-                            :counter="10"
                             label="password"
+                            @input="removeError('password')"
+                            :error-messages="errors.password"
+                            type="password"
                             required
                         ></v-text-field>
                         <v-text-field
                             v-model="form.confirmpassword"
-                            :counter="10"
                             label="confirm password"
+                            @input="removeError('confirmpassword')"
+                            :error-messages="errors.confirmpassword"
+                            type="password"
                             required
                         ></v-text-field>
                     </v-stepper-content>
@@ -45,7 +55,6 @@
                     <v-stepper-content step="2">
                         <v-text-field
                             v-model="form.email"
-                            :counter="50"
                             label="E-mail"
                             required
                         ></v-text-field>
@@ -86,10 +95,17 @@ export default {
     data() {
         return {
             form: {
-                id: '',
+                username: '',
                 password: '',
                 confirmpassword: '',
                 realname: '',
+                email: '',
+            },
+            errors: {
+                username: '',
+                realname: '',
+                password: '',
+                confirmpassword: '',
                 email: '',
             },
             curpage: 1,
@@ -100,9 +116,24 @@ export default {
         maxpage() {
             return 2
         },
+        errorRequiredStep() {
+            if (
+                this.errors.username != '' ||
+                this.errors.realname != '' ||
+                this.errors.password != '' ||
+                this.errors.confirmpassword != ''
+            ) {
+                return true
+            }
+            return false
+        },
     },
     methods: {
         async finish() {
+            this.clearErrors()
+            if (!this.checkValid()) {
+                return
+            }
             this.isloading = true
             try {
                 const res = await axios.post('auth/register', {
@@ -116,6 +147,40 @@ export default {
             } finally {
                 this.isloading = false
             }
+        },
+        checkValid() {
+            let result = true
+            if (this.form.username == '') {
+                this.errors.username = '아이디를 입력해주세요'
+                result = false
+            }
+            if (this.form.realname == '') {
+                this.errors.realname = '이름을 입력해주세요'
+                result = false
+            }
+            if (this.form.password == '') {
+                this.errors.password = '비밀번호를 입력해주세요'
+                result = false
+            }
+            if (this.form.confirmpassword == '') {
+                this.errors.confirmpassword = '비밀번호를 입력해주세요'
+                result = false
+            }
+            if (this.form.password !== this.form.confirmpassword) {
+                this.errors.confirmpassword =
+                    '비밀번호 확인이 일치하지 않습니다.'
+                result = false
+            }
+            return result
+        },
+        clearErrors() {
+            const keys = Object.keys(this.errors)
+            keys.forEach(i => {
+                this.errors[i] = ''
+            })
+        },
+        removeError(field) {
+            this.errors[field] = ''
         },
     },
 }
