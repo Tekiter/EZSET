d00373039e4a9bba1ebc2638e4e87dd92059541a
ip99202 <ip99202@naver.com>
2019-12-30 20:42:57 +0900

    Create comment model

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -2,6 +2,17 @@ import mongoose from 'mongoose'
 import autoIncrement from 'mongoose-auto-increment'
 const Schema = mongoose.Schema
 
+let commentSchema = new Schema({
+    content: { type: String, required: [true, 'comment content required'] },
+    writer: { type: String, required: [true, 'comment writer required'] },
+})
+
+commentSchema.plugin(autoIncrement.plugin, {
+    model: 'comment',
+    startAt: 1,
+})
+//let Comment = mongoose.model('comment', commentSchema)
+
 let postSchema = new Schema({
     board: {
         type: Number,
@@ -19,6 +30,15 @@ let postSchema = new Schema({
         type: String,
         required: [true, 'post writer required'],
     },
+    created_date: {
+        type: Date,
+        default: Date.now,
+    },
+    cnt: {
+        view: { type: Number, default: 0 },
+        like: { type: Number, default: 0 },
+    },
+    comments: [commentSchema],
 })
 
 postSchema.plugin(autoIncrement.plugin, 'post')
