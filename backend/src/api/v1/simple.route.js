df2f492c64acabd805ca710eeced43a571f463a1
ip99202 <ip99202@naver.com>
2020-01-06 21:50:43 +0900

    Complete code clean up

--- backend/src/api/v1/simple.route.js
+++ backend/src/api/v1/simple.route.js
@@ -1,42 +1,51 @@
 import { Router } from 'express'
 import { body, param } from 'express-validator'
 import Board from '../../models/Board'
-const Post = require('../../models/Post')
+import Post from '../../models/Post'
+import { validateParams, asyncRoute } from '../../utils/api'
 const router = Router()
 
 //post board
-router.post('/boards', [body('title').isString()], function(req, res) {
+router.post('/boards', [body('title').isString(), validateParams], function(
+    req,
+    res
+) {
     let board = new Board()
     board.title = req.body.title
 
     board.save(function(err) {
         if (err) {
-            //console.error(err)
-            res.status(503).json({ error })
-            return
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
         }
         res.status(201).json({ board })
     })
 })
 
 //delete board
-router.delete('/boards/:board_id', [param('board_id').isNumeric()], function(
-    req,
-    res
-) {
-    Board.remove({ _id: req.params.board_id }, function(err, output) {
-        if (err) {
-            return res.status(500).json({ error: 'database failure' })
-        }
-        res.status(204).end()
-    })
-})
+router.delete(
+    '/boards/:board_id',
+    [param('board_id').isNumeric(), validateParams],
+    function(req, res) {
+        Board.remove({ _id: req.params.board_id }, function(err, output) {
+            if (err) {
+                const errr = new Error('database error')
+                errr.status = 500
+                throw errr
+            }
+            res.status(204).end()
+        })
+    }
+)
 
 //get all board
 router.get('/boards', function(req, res) {
     Board.find(function(err, board) {
         if (err) {
-            return res.status(500).send({ err: 'database failure' })
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
         }
         res.json(board)
     })
@@ -49,184 +58,200 @@ router.post(
         param('board_id').isNumeric,
         body('title').isString(),
         body('content').isString(),
+        validateParams,
     ],
-    function(req, res) {
+    asyncRoute(async function(req, res) {
         if (!req.perm('board', req.params.board_id).can('create')) {
             return
         }
         const boardId = parseInt(req.params.board_id)
-        Board.findOne()
-            .where('_id')
-            .equals(boardId)
-            .then(board => {
-                const post = new Post({
-                    board: boardId,
-                    title: req.body.title,
-                    content: req.body.content,
-                    author: req.user.username,
-                })
-                post.save()
-                    .then(newpost => {
-                        res.status(201).json(newpost)
-                    })
-                    .catch(() => res.json({ error }))
+
+        try {
+            let board = await Board.findOne()
+                .where('_id')
+                .equals(boardId)
+
+            const post = new Post({
+                board: boardId,
+                title: req.body.title,
+                content: req.body.content,
+                author: req.user.username,
             })
-            .catch(err => {
-                //console.log(err)
-                res.status(404).json({ message: 'no board id ' + boardId })
-            })
-    }
+
+            let newpost = await post.save()
+
+            res.status(201).json(newpost)
+        } catch (err) {
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
+        }
+    })
 )
 
 //delete post
-router.delete('/posts/:post_id', [param('post_id').isNumeric()], function(
-    req,
-    res
-) {
-    if (!req.perm('board', req.params.board_id).canOwn('delete')) {
-    }
+router.delete(
+    '/posts/:post_id',
+    [param('post_id').isNumeric(), validateParams],
+    asyncRoute(async function(req, res) {
+        if (!req.perm('board', req.params.board_id).canOwn('delete')) {
+        }
 
-    Post.findById(req.params.post_id)
-        .then(post => {
+        try {
+            let post = await Post.findById(req.params.post_id)
             if (post) {
                 if (post.owner != req.user.username) {
                 }
 
-                post.delete()
-                    .then(() => {
-                        res.status(200).json({
-                            message: 'post deleted',
-                            target: post,
-                        })
-                    })
-                    .catch(() => res.json({ error }))
+                await post.delete()
+                res.status(200).json({ message: 'post deleted', target: post })
             } else {
                 res.status(404).json({
                     message: 'no post id ' + req.params.post_id,
                 })
             }
-        })
-        .catch(() => res.json({ error }))
-})
+        } catch (error) {
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
+        }
+    })
+)
 
 //edit post
 router.put(
     '/posts/:post_id',
-    [param('post_id').isNumeric(), body('content').isString()],
-    function(req, res) {
-        Post.findById(req.params.post_id).then(post => {
+    [param('post_id').isNumeric(), body('content').isString(), validateParams],
+    asyncRoute(async function(req, res) {
+        let post = await Post.findById(req.params.post_id)
+
+        try {
             if (post) {
                 if (req.body.content) {
                     post.content = req.body.content
                 }
-                post.save()
-                    .then(() => {
-                        res.status(200).json({
-                            message: '수정 완료',
-                            target: post,
-                        })
-                    })
-                    .catch(() => res.json({ error }))
+                await post.save()
+
+                res.status(200).json({
+                    message: '수정 완료',
+                    target: post,
+                })
             } else {
                 res.status(404).json({
                     message: 'no post id' + req.params.post_id,
                 })
             }
-        })
-    }
+        } catch (error) {
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
+        }
+    })
 )
 
 //get one post
-router.get('/posts/:post_id', [param('post_id').isNumeric()], function(
-    req,
-    res
-) {
-    Post.findOne()
-        .where('_id')
-        .equals(req.params.post_id)
-        .then(post => {
-            if (post) res.status(200).json(post)
-            else
-                res.status(404).json({
-                    message: 'no post id ' + req.params.post_id,
-                })
-        })
-})
-
-//get all post (get one board)
-router.get('/boards/:board_id', [param('board_id').isNumeric], function(
-    req,
-    res
-) {
-    let boardId = parseInt(req.params.board_id)
-    Board.findOne({ _id: boardId })
-        .then(board => {
-            if (!board) {
-                res.status(404).json({ message: 'no board id ' + boardId })
-                return
-            }
-            Post.find()
-                .where('board')
-                .equals(boardId)
-                .then(posts => {
-                    res.status(200).json({
-                        board: board,
-                        posts: posts,
-                    })
-                })
-                .catch(() => res.json({ error: error }))
-        })
-        .catch(() => res.json({ error: error }))
-})
-
-//post comment
-router.post(
-    '/posts/:post_id/comment',
-    [param('post_id').isNumeric(), body('comment').isString()],
+router.get(
+    '/posts/:post_id',
+    [param('post_id').isNumeric(), validateParams],
     function(req, res) {
         Post.findOne()
             .where('_id')
             .equals(req.params.post_id)
             .then(post => {
-                if (!post) {
+                if (post) res.status(200).json(post)
+                else
                     res.status(404).json({
-                        message: 'no post id' + req.params.post_id,
+                        message: 'no post id ' + req.params.post_id,
                     })
-                    return
-                }
-                post.addComment(req.body.content, req.body.user)
-                    .then(() => {
-                        res.status(201).json({ message: '댓글 작성 완료' })
-                    })
-                    .catch(() => res.json({ message: 'function error' }))
             })
-        //.catch(e => console.log(e))
     }
+)
+
+//get all post (get one board)
+router.get(
+    '/boards/:board_id',
+    [param('board_id').isNumeric(), validateParams],
+    asyncRoute(async function(req, res) {
+        let boardId = parseInt(req.params.board_id)
+
+        try {
+            let board = await Board.findOne({ _id: boardId })
+            if (!board) {
+                res.status(404).json({ message: 'no board id ' + boardId })
+                return
+            }
+
+            let posts = await Post.find()
+                .where('board')
+                .equals(boardId)
+            res.status(200).json({
+                board: board,
+                posts: posts,
+            })
+        } catch (error) {
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
+        }
+    })
+)
+
+//post comment
+router.post(
+    '/posts/:post_id/comment',
+    [param('post_id').isNumeric(), body('comment').isString(), validateParams],
+    asyncRoute(async function(req, res) {
+        try {
+            let post = await findOne()
+                .where('_id')
+                .equals(req.params.post_id)
+
+            if (!post) {
+                res.status(404).json({
+                    message: 'no post id' + req.params.post_id,
+                })
+                return
+            }
+
+            await post.addComment(req.body.content, req.body.user)
+            res.status(201).json({ message: '댓글 작성 완료' })
+        } catch (error) {
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
+        }
+    })
 )
 
 //delete comment
 router.delete(
     '/posts/:post_id/comment/:comment_id',
-    [param('post_id').isNumeric(), param('comment_id').isNumeric()],
-    function(req, res) {
-        Post.findOne()
-            .where('_id')
-            .equals(req.params.post_id)
-            .then(post => {
-                if (!post) {
-                    res.status(404).json({
-                        message: 'no post id ' + req.params.comment_id,
-                    })
-                    return
-                }
-                post.removeComment(req.params.comment_id)
-                    .then(() => {
-                        res.status(200).json({ message: '삭제 성공' })
-                    })
-                    .catch(() => res.json({ message: 'remove function error' }))
-            })
-            .catch(() => res.json({ message: 'find error' }))
-    }
+    [
+        param('post_id').isNumeric(),
+        param('comment_id').isNumeric(),
+        validateParams,
+    ],
+    asyncRoute(async function(req, res) {
+        try {
+            let post = await Post.findOne()
+                .where('_id')
+                .equals(req.params.post_id)
+
+            if (!post) {
+                res.status(404).json({
+                    message: 'no post id ' + req.params.comment_id,
+                })
+                return
+            }
+
+            await post.removeComment(req.params.comment_id)
+            res.status(200).json({ message: '삭제 성공' })
+        } catch (error) {
+            const errr = new Error('database error')
+            errr.status = 500
+            throw errr
+        }
+    })
 )
 
 export default router
