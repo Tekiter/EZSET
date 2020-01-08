1571ba16c657700e7c11d745a481fe64cec5c2d8
vividswan <vividswan@naver.com>
2020-01-08 20:24:06 +0900

    Add axios function to Board.vue

--- frontend/src/views/Board.vue
+++ frontend/src/views/Board.vue
@@ -2,23 +2,36 @@
     <v-data-table
         :headers="headers"
         :items="boards"
-        :items-per-page="5"
+        :items-per-page="10"
         class="elevation-1"
     >
         <template v-slot:top>
             <v-divider class="mx-4" inset vertical></v-divider>
 
             <v-toolbar-title class="d-flex justify-center"
-                >-게시판 이름-</v-toolbar-title
+                >-{board.title}-</v-toolbar-title
             >
             <v-divider class="mx-2" inset vertical></v-divider>
         </template>
     </v-data-table>
 </template>
 <script>
+import axios from 'axios'
 export default {
+    mounted() {
+        axios
+            .get(`http://localhost:5000/api/v1/simple/boards/${this.board._id}`)
+            .then(r => {
+                console.log(r), (this.board = r.board), (this.posts = r.posts)
+            })
+            .catch(e => {
+                console.error(e.message)
+            })
+    },
     data() {
         return {
+            board: '',
+            posts: '',
             headers: [
                 {
                     text: '번호',
@@ -26,21 +39,16 @@ export default {
                     sortable: false,
                     value: 'number',
                 },
-                { text: '제목', value: 'title' },
-                { text: '작성자', value: 'author' },
-                { text: '작성일', value: 'date' },
-                { text: '추천', value: 'recommands' },
-                { text: '조회', value: 'hits' },
-            ],
-            boards: [
                 {
-                    number: 123,
-                    title: '안녕',
-                    author: '박수환',
-                    date: '12/31',
-                    recommands: 4,
-                    hits: 23,
+                    text: '제목',
+                    value: 'posts.title',
+                    sortable: false,
+                    width: '50%',
                 },
+                { text: '작성자', value: 'posts.author', sortable: false },
+                { text: '작성일', value: 'posts.created_date' },
+                { text: '추천', value: 'posts.like' },
+                { text: '조회', value: 'posts.view' },
             ],
         }
     },
