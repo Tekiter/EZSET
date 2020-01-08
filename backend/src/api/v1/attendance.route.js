f0184e93de034d74ab7b1ca1757759854aa64d64
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-01-08 21:18:08 +0900

    Remake attendanceWrite API

--- backend/src/api/v1/attendance.route.js
+++ backend/src/api/v1/attendance.route.js
@@ -1,6 +1,8 @@
 import Router from 'express'
 import { asyncRoute } from '../../utils/api'
 import random from 'random-number-csprng'
+import User from '../../models/User'
+
 const AttendanceDay = require('../../models/AttendanceDay')
 const AttendanceUser = require('../../models/AttendanceUser')
 const router = Router()
@@ -76,16 +78,50 @@ router.get(
     })
 )
 
-router.get(
+router.post(
     '/startAttendance',
     asyncRoute(async function(req, res) {
+        var Date = moment().format('YYYYMMDD')
+        //get Userlist in User collection
+        const userList = await User.find()
+            .where('day')
+            .equals(req.body.date)
+            .select('username')
+        //create db - AttendanceDay
+        var attendanceDay = new AttendanceDay()
+        attendanceDay.day = Date
+        for (var k in userList) {
+            //create db - AttendanceDay
+            var cursor_Day = await AttendanceDay.findOne()
+                .where('day')
+                .equals(Date)
+            if (!cursor_Day) {
+                var attendanceDay = new AttendanceDay()
+                attendanceDay.day = Date
+                attendanceDay.addStatus(userList[k].username, 'absence')
+            } else {
+                cursor_Day.addStatus(userList[k].username, 'absence')
+            }
+            //create db - AttendanceUser
+            var cursor_User = await AttendanceUser.findOne()
+                .where('name')
+                .equals(userList[k].username)
+            if (!cursor_User) {
+                var attendanceUser = new AttendanceUser()
+                attendanceUser.name = userList[k].username
+                attendanceUser.addStatus(Date, 'absence')
+            } else {
+                cursor_User.addStatus(Date, 'absence')
+            }
+        }
+        //Generate Code
         try {
             ranNum = await random(100, 999)
             res.json({ code: ranNum })
         } catch (err) {
-            console.log(err) // eslint-disable-line no-console
             res.status(501).json
         }
     })
 )
+
 export default router
