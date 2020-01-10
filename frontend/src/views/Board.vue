945b06a5d05640b2c34efc27549c2b4257815cee
vividswan <vividswan@naver.com>
2020-01-10 19:12:29 +0900

    Add Moment

--- frontend/src/views/Board.vue
+++ frontend/src/views/Board.vue
@@ -1,51 +1,41 @@
 <template>
-    <v-data-table :headers="headers" :items="posts" :items-per-page="10" class="elevation-1">
-        <template v-slot:top>
-            <v-divider class="mx-4" inset vertical></v-divider>
-
-            <v-toolbar-title class="d-flex justify-center">-{{ board.title }}-</v-toolbar-title>
-            <v-divider class="mx-2" inset vertical></v-divider>
-        </template>
-    </v-data-table>
+    <v-data-table
+        :headers="header"
+        :items="boards"
+        :items-per-page="5"
+        class="elevation-1"
+        @click:row="read"
+    ></v-data-table>
 </template>
+
 <script>
 import axios from 'axios'
 export default {
-    mounted() {
-        axios
-            .get('/simple/boards/' + this.$route.params.board_id)
-            .then(res => {
-                this.posts = res.data.posts
-                this.board = res.data.board
-                console.log(res.data.posts)
-            })
-            .catch(e => {
-                console.error(e.message)
-            })
-    },
     data() {
         return {
-            board: '',
-            posts: [],
-            headers: [
+            header: [
                 {
-                    text: '번호',
-                    align: 'left',
-                    sortable: false,
-                    value: 'number',
-                },
-                {
-                    text: '제목',
+                    text: '게시판 이름',
                     value: 'title',
-                    sortable: false,
-                    width: '50%',
                 },
-                { text: '작성자', value: 'author', sortable: false },
-                { text: '작성일', value: 'created_date' },
-                { text: '추천', value: 'like' },
-                { text: '조회', value: 'view' },
             ],
+            boards: [],
         }
+    },
+    methods: {
+        read(evt) {
+            //evt라는 임의의 변수안에 board 정보 들어가있음
+            console.log(evt._id)
+            console.log(evt.title)
+            this.$router.push({
+                path: '/board/' + evt._id + '/' + evt.title,
+            })
+        },
+    },
+    mounted() {
+        axios.get('/simple/boards').then(res => {
+            this.boards = res.data
+        })
     },
 }
 </script>
