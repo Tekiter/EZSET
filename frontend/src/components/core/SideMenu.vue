a5cca2621905f6d49822288351c4df6980640171
Tekiter <tekiter8@gmail.com>
2020-01-11 04:21:50 +0900

    Complete side menu tree

--- frontend/src/components/core/SideMenu.vue
+++ frontend/src/components/core/SideMenu.vue
@@ -1,20 +1,10 @@
 <template>
     <v-list dense>
-        <template v-for="(menu, index) in mainMenus">
-            <!-- <v-list-item
-                v-if="menu.type == 'simple' && menu.perm != false"
-                :key="index"
-                :to="menu.to"
-            >
-                <v-list-item-action>
-                    <v-icon>{{ menu.icon }}</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                    <v-list-item-title>{{ menu.title }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item> -->
-            <side-menu-item :options="menu" :key="index" />
-        </template>
+        <side-menu-item
+            v-for="(menu, index) in mainMenus"
+            :options="menu"
+            :key="index"
+        />
     </v-list>
 </template>
 <style scoped></style>
@@ -36,18 +26,12 @@ export default {
                     type: 'simple',
                     icon: 'mdi-settings',
                     title: '설정',
-                    to: '/settings',
-                    perm: this.$perm('role').can('read'),
+
                     children: [
                         {
-                            title: '내부설정',
-                            to: '/innerset',
-                            children: [
-                                {
-                                    title: '또다른 내부',
-                                    to: '/innn',
-                                },
-                            ],
+                            title: '유저 관리',
+                            to: '/manage/users',
+                            perm: this.$perm('role').can('read'),
                         },
                     ],
                 },
