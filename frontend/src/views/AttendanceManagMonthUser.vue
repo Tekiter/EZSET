fe050bbf9cd1c7ce51d132732437ff6297924260
small-j <smallj1044@gmail.com>
2020-01-29 21:05:09 +0900

    Update AttendanceManagMonthUser.vue dialog button

--- frontend/src/views/AttendanceManagMonthUser.vue
+++ frontend/src/views/AttendanceManagMonthUser.vue
@@ -15,91 +15,91 @@
             v-if="this.$perm('attendance').canOwn('read')"
             class="font-weight-medium subtitle-2"
         >
-            <div>
-                <!-- 이 div는 결석예약을 위한 다이얼로그를 띄우는 div입니다.  -->
-                <form>
-                    <v-dialog
-                        v-model="absenceResDialog.show"
-                        persistent
-                        max-width="290"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <v-btn color="primary" dark v-on="on"
-                                >결석예약</v-btn
-                            >
-                        </template>
-                        <v-card>
-                            <v-date-picker
-                                v-model="dates"
-                                multiple
-                            ></v-date-picker>
-                            <v-menu
-                                ref="menu"
-                                v-model="menu"
-                                :close-on-content-click="false"
-                                :return-value.sync="dates"
-                                transition="scale-transition"
-                                offset-y
-                                full-width
-                                min-width="290px"
-                            >
-                                <template v-slot:activator="{ on }">
-                                    <v-combobox
-                                        v-model="dates"
-                                        multiple
-                                        chips
-                                        small-chips
-                                        label="Multiple picker in menu"
-                                        prepend-icon="mdi-plus"
-                                        readonly
-                                        v-on="on"
-                                    ></v-combobox>
-                                </template>
+            <v-container>
+                <div class="d-flex fles-row mb-6">
+                    <!-- 이 div는 결석예약을 위한 다이얼로그를 띄우는 div입니다.  -->
+                    <form>
+                        <v-dialog
+                            v-model="absenceResDialog.show"
+                            persistent
+                            max-width="290"
+                        >
+                            <template v-slot:activator="{ on }">
+                                <v-btn color="primary" dark v-on="on"
+                                    >결석예약</v-btn
+                                >
+                            </template>
+                            <v-card>
                                 <v-date-picker
                                     v-model="dates"
                                     multiple
-                                    no-title
-                                    scrollable
+                                ></v-date-picker>
+                                <v-menu
+                                    ref="menu"
+                                    v-model="menu"
+                                    :close-on-content-click="false"
+                                    :return-value.sync="dates"
+                                    transition="scale-transition"
+                                    offset-y
+                                    full-width
+                                    min-width="290px"
                                 >
+                                    <template v-slot:activator="{ on }">
+                                        <v-combobox
+                                            v-model="dates"
+                                            multiple
+                                            chips
+                                            small-chips
+                                            label="Multiple picker in menu"
+                                            prepend-icon="mdi-plus"
+                                            readonly
+                                            v-on="on"
+                                        ></v-combobox>
+                                    </template>
+                                    <v-date-picker
+                                        v-model="dates"
+                                        multiple
+                                        no-title
+                                        scrollable
+                                    >
+                                        <v-spacer></v-spacer>
+                                        <v-btn
+                                            text
+                                            color="primary"
+                                            @click="menu = false"
+                                            >Cancel</v-btn
+                                        >
+                                        <v-btn
+                                            text
+                                            color="primary"
+                                            @click="$refs.menu.save(dates)"
+                                            >OK</v-btn
+                                        >
+                                    </v-date-picker>
+                                </v-menu>
+                                <v-text-field
+                                    label="결석사유"
+                                    v-model="absence_reason"
+                                ></v-text-field>
+                                <v-card-actions>
                                     <v-spacer></v-spacer>
                                     <v-btn
+                                        color="green darken-1"
                                         text
-                                        color="primary"
-                                        @click="menu = false"
-                                        >Cancel</v-btn
+                                        @click="absenceResDialog.show = false"
+                                        >취소</v-btn
                                     >
                                     <v-btn
+                                        color="green darken-1"
                                         text
-                                        color="primary"
-                                        @click="$refs.menu.save(dates)"
-                                        >OK</v-btn
+                                        @click="reservation"
+                                        >확인</v-btn
                                     >
-                                </v-date-picker>
-                            </v-menu>
-                            <v-text-field
-                                label="결석사유"
-                                v-model="absence_reason"
-                            ></v-text-field>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn
-                                    color="green darken-1"
-                                    text
-                                    @click="absenceResDialog.show = false"
-                                    >취소</v-btn
-                                >
-                                <v-btn
-                                    color="green darken-1"
-                                    text
-                                    @click="reservation"
-                                    >확인</v-btn
-                                >
-                            </v-card-actions>
-                        </v-card>
-                    </v-dialog>
-                </form>
-            </div>
-            <v-container>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                    </form>
+                </div>
                 <v-row class="fill-height">
                     <v-col>
                         <v-sheet height="64">
