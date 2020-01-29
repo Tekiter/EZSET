8fe31ba090c3c19190e428d1b9ac838e90d5c26d
small-j <smallj1044@gmail.com>
2020-01-29 21:10:16 +0900

    Update OfficialAbsenceAccept.vue

--- frontend/src/views/OfficialAbsenceAccept.vue
+++ frontend/src/views/OfficialAbsenceAccept.vue
@@ -1,5 +1,42 @@
 <template>
-    <v-container> </v-container>
+    <v-container>
+        <v-card max-width="344">
+            <v-card-text>
+                <div>Word of the Day</div>
+                <p class="display-1 text--primary">
+                    be•nev•o•lent
+                </p>
+                <p>adjective</p>
+                <div class="text--primary">
+                    well meaning and kindly.<br />
+                    "a benevolent smile"
+                </div>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn text color="deep-purple accent-4">
+                    Learn More
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+        <v-card class="mx-auto" max-width="344">
+            <v-card-text>
+                <div>Word of the Day</div>
+                <p class="display-1 text--primary">
+                    be•nev•o•lent
+                </p>
+                <p>adjective</p>
+                <div class="text--primary">
+                    well meaning and kindly.<br />
+                    "a benevolent smile"
+                </div>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn text color="deep-purple accent-4">
+                    Learn More
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-container>
 </template>
 <script>
 export default {
