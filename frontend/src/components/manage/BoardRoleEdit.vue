0cb0465a042a20b45bab88fcd8d7d4ca1c5a7046
vividswan <vividswan@naver.com>
2020-02-26 16:50:49 +0900

    Feat board role edit

--- frontend/src/components/manage/BoardRoleEdit.vue
+++ frontend/src/components/manage/BoardRoleEdit.vue
@@ -1,17 +1,91 @@
 <template>
-    <v-card class="d-flex">
-        <v-row>
-            <v-col>
-                1111
-            </v-col>
-            <v-divider vertical></v-divider>
-            <v-col>
-                2222
-            </v-col>
-        </v-row>
-    </v-card>
+    <div>
+        <v-card class="mx-auto" max-width="400" tile>
+            <v-list flat>
+                <v-subheader>
+                    <v-col></v-col>
+                    <v-divider class="mx-4" vertical></v-divider>
+                    <v-col class="title black--text">보기</v-col>
+                    <v-divider class="mx-4" vertical></v-divider>
+                    <v-col class="title black--text">쓰기</v-col>
+                    <v-divider class="mx-4" vertical></v-divider>
+                    <v-col class="title black--text">삭제</v-col>
+                </v-subheader>
+
+                <v-list-item v-for="(item, i) in items" :key="i">
+                    <v-list-item-content>
+                        <v-list-item-title
+                            class="text-center subtitle-1"
+                            v-text="item.status"
+                        ></v-list-item-title>
+                    </v-list-item-content>
+                    <v-divider class="mx-4" vertical></v-divider>
+                    <v-list-item-content class="d-flex justify-center">
+                        <v-switch v-model="item.read"></v-switch>
+                    </v-list-item-content>
+                    <v-divider class="mx-4" vertical></v-divider>
+                    <v-list-item-content class="d-flex justify-center">
+                        <v-switch
+                            class="d-flex justify-center"
+                            v-model="item.write"
+                        ></v-switch>
+                    </v-list-item-content>
+                    <v-divider class="mx-4" vertical></v-divider>
+                    <v-list-item-content class="d-flex justify-center">
+                        <v-switch
+                            class="d-flex justify-center"
+                            v-model="item.delete"
+                        ></v-switch>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-card>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    status: '시니어',
+                    read: true,
+                    write: true,
+                    delete: false,
+                },
+                {
+                    status: '시니어',
+                    read: true,
+                    write: true,
+                    delete: false,
+                },
+                {
+                    status: '시니어',
+                    read: true,
+                    write: true,
+                    delete: false,
+                },
+                {
+                    status: '시니어',
+                    read: true,
+                    write: true,
+                    delete: false,
+                },
+                {
+                    status: '시니어',
+                    read: true,
+                    write: true,
+                    delete: false,
+                },
+                {
+                    status: '시니어',
+                    read: true,
+                    write: true,
+                    delete: false,
+                },
+            ],
+        }
+    },
+}
 </script>
