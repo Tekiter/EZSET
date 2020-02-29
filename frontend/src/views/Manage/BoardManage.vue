4ce5b0999ef492b0ecd4e4b4b8ebd78409605980
Tekiter <tekiter8@gmail.com>
2020-03-01 01:52:49 +0900

    Remove unused button

--- frontend/src/views/Manage/BoardManage.vue
+++ frontend/src/views/Manage/BoardManage.vue
@@ -149,9 +149,6 @@
         </v-dialog>
         <v-row justify="center">
             <v-dialog v-model="modifyDialog" persistent max-width="290">
-                <template v-slot:activator="{ on }">
-                    <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-                </template>
                 <v-card>
                     <v-card-title class="headline"
                         >게시판 이름 수정</v-card-title
