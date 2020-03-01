b62351351b967252f567d7e2891357374302735c
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-03-01 18:04:23 +0900

    Modify directory

--- tools/update.sh
+++ tools/update.sh
@@ -16,7 +16,7 @@ echo " PORT : $PORT"
 echo " SOCKET_PORT : $SOCKET_PORT"
 echo " JWT_SECRET : $JWT_SECRET"
 echo "====start service===="
-cd ./../frontend
+cd ./frontend
 yarn install
 yarn build
 cd ./../backend
@@ -25,5 +25,6 @@ yarn build
 yarn start
 echo "====================="
 echo "Server will reboot every 7days"
-sleep 7d
 echo "====================="
+sleep 7d
+exit()
