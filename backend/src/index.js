b1ecf0343e8dd05d2f86367c01e1f9b12d419b7a
Tekiter <tekiter8@gmail.com>
2020-03-13 14:56:27 +0900

    Increase file upload size

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,4 @@
 // /* eslint-disable no-console */
-// import database from './utils/database'
-// import app from './app'
-// import initialization from './utils/initialization'
-
-// const { PORT = 8080 } = process.env
-
-// async function initApp() {
-//     try {
-//         await database.initialize()
-//         console.log('Successfully connected to database')
-
-//         await initialization.initialize()
-
-//         app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-//     } catch (error) {
-//         console.log('Failed to initialize')
-//         console.error(error)
-//     }
-// }
-
-// initApp()
-
-// // database
-// //     .initialize()
-// //     .then(() => {
-// //         console.log('Successfully connected to database')
-// //         app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
-// //     })
-// //     .catch(error => {
-// //         console.log('Failed to connect database')
-// //         console.error(error)
-// //     })
 
 import initApp from './init'
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -26,6 +26,8 @@ Vue.prototype.$action = actionHelper
 
 // api 호출의 기본 경로
 axios.defaults.baseURL = '/api/v1'
+axios.defaults.maxContentLength = 10000000
+axios.defaults.maxBodyLength = 10000000
 
 axios.interceptors.response.use(
     function(response) {
