4bba4b1ed633c93e0e75bec92e03dc28252a657d
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-03-16 03:46:19 +0900

    Create penalty API

--- backend/src/api/v1/penalty.route.js
+++ backend/src/api/v1/penalty.route.js
@@ -1,21 +1,100 @@
 import Router from 'express'
 import { asyncRoute, validateParams, checkUsername } from '../../utils/api'
-import User from '../../models/User'
 import Penalty from '../../models/Penalty/Penalty'
-import PenaltyConfig from '../../models/Penalty/PenaltyConfig'
 import { perm } from '../../utils/role'
 import { param, body } from 'express-validator'
 const router = Router()
+var moment = require('moment')
 
 //get penalty
+// penalty.canOwn('read')
 router.get(
-    '/read', [perm('penalty').canOwn('read')],
+    '/read', [
+        perm('attendance').can('update'),
+        body('start_date').isString(),
+        body('end_date').isString(),
+        body('username').isString(),
+        validateParams,
+    ],
     asyncRoute(async function(req, res) {
-        const cursor = await PenaltyConfig.find()
-        res.json(cursor)
+        var penalty = await Penalty.find({
+            date: {
+                $gte: moment(req.body.start_date).format('YYYY-MM-DD'),
+                $lte: moment(req.body.end_date).format('YYYY-MM-DD'),
+            },
+        }).sort({ date: 1 })
+        res.send(penalty)
     })
 )
 
 //write config
-router.post('/write')
+router.post(
+    '/write', [
+        perm('attendance').can('update'),
+        body('type').isString(),
+        body('date').isString(),
+        body('username').isString(),
+        body('description').isString(),
+        validateParams,
+    ],
+    asyncRoute(async function(req, res) {
+        var penalty = new Penalty()
+        penalty.type = req.body.type
+        penalty.username = req.body.username
+        penalty.date = req.body.date
+        penalty.description = req.body.description
+
+        await penalty.save()
+        res.end()
+    })
+)
+
+//delete config
+router.post(
+    '/delete', [
+        perm('attendance').can('update'),
+        body('type').isString(),
+        body('date').isString(),
+        body('username').isString(),
+        body('description').isString(),
+        validateParams,
+    ],
+    asyncRoute(async function(req, res) {
+        await Penalty.findOneAndDelete({
+            type: req.body.type,
+            username: req.body.username,
+            date: req.body.date,
+            description: req.body.description,
+        })
+        res.end()
+    })
+)
+
+//delete config
+router.post(
+    '/update', [
+        perm('attendance').can('update'),
+        body('type').isString(),
+        body('date').isString(),
+        body('username').isString(),
+        body('description').isString(),
+        body('ntype').isString(),
+        body('ndate').isString(),
+        body('ndescription').isString(),
+        validateParams,
+    ],
+    asyncRoute(async function(req, res) {
+        await Penalty.findOneAndUpdate({
+            type: req.body.type,
+            username: req.body.username,
+            date: req.body.date,
+            description: req.body.description,
+        }, {
+            type: req.body.ntype,
+            date: req.body.ndate,
+            description: req.body.ndescription,
+        })
+        res.end()
+    })
+)
 export default router
