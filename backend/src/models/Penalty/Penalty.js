c90da4a0968b45f371be21d9335e355832d1c783
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-03-20 16:42:01 +0900

    Change Penalty Schema

--- backend/src/models/Penalty/Penalty.js
+++ backend/src/models/Penalty/Penalty.js
@@ -1,17 +1,25 @@
 import mongoose from 'mongoose'
 
 const penaltySchema = new mongoose.Schema({
+    type_id: {
+        type: String,
+        index: true,
+    },
     type: {
         type: String,
+        trim: true,
     },
     username: {
         type: String,
+        trim: true,
     },
     date: {
         type: Date,
+        trim: true,
     },
     description: {
         type: String,
+        trim: true,
     },
 })
 
