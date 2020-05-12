13f7449e4a4e765e790e343326874aa6996369c6
최현석 <43382559+hschoi1104@users.noreply.github.com>
2020-05-12 15:56:26 +0900

    Fix Upload dir

--- tools/update.sh
+++ tools/update.sh
@@ -1,30 +1,26 @@
-(update.sh)
+#(update.sh)
 #!/bin/bash
+cd EZSET
 echo "====PULL EZSET===="
-cd ./../
-git fetch --all
-git reset --hard origin/master
-git pull
+#git clone -b release --single-branch https://github.com/Tekiter/EZSET.git
+git pull origin release
 echo "====ENV SETTING===="
 sudo iptables -I INPUT -p tcp --dport 5050 -j ACCEPT
 export DATABASE_URI=mongodb://localhost:27017/ezset
 export PORT=5000
 export SOCKET_PORT=5050
-export JWT_SECRET=somesecretwowowowokasjdk
+export JWT_SECRET=EZSET_SECREAT_Key
+export FILE_UPLOAD_DIR=./../upload
 echo " DATABASE_URI : $DATABASE_URI"
 echo " PORT : $PORT"
 echo " SOCKET_PORT : $SOCKET_PORT"
 echo " JWT_SECRET : $JWT_SECRET"
+echo " FILE_UPLOAD_DIR : $FILE_UPLOAD_DIR"
 echo "====start service===="
-cd ./frontend
 yarn install
-yarn build
-cd ./../backend
-yarn install
-yarn build
 yarn start
 echo "====================="
 echo "Server will reboot every 7days"
 echo "====================="
-sleep 7d
-exit()
+#sleep 7d
+#exit()
