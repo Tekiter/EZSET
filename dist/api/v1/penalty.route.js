4a7902171b64e696d7171a9353f4d3512a33ab37
hschoi1104 <43382559+hschoi1104@users.noreply.github.com>
2020-05-27 13:24:12 +0000

    Update release to output generated at 0bd4865

--- dist/api/v1/penalty.route.js
+++ dist/api/v1/penalty.route.js
@@ -124,20 +124,102 @@ router.get('/read/:username', [(0, _role.perm)('penalty').can('read'), (0, _expr
 }));
 
 /**
+ * @api {get} /penalty/read 상벌점 전체 조회
+ * @apiDescription 전체 상벌점 조회
+ * @apiName penaltyReadAll
+ * @apiGroup Penalty
+ * @apiPermission penalty.can.read
+ *
+ *
+ * @apiSuccess {Array} - 사용자의 상벌점 항목을 배열로 반환
+ * @apiSuccess {String} type 상벌점 항목의 타입
+ * @apiSuccess {String} username 상벌점 항목의 사용자
+ * @apiSuccess {String} date 상벌점 항목이 기록된 날짜
+ * @apiSuccess {String} description 상벌점 항목의 설명
+ * @apiSuccess {Number} point 상벌점 항목의 점수
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          [{
+ *              type:"지각",
+ *              username:"admin"
+ *              date:"2020-03-19",
+ *              description:"지각",
+ *              point:-1
+ *          }]
+ *      }
+ */
+router.get('/read', [(0, _role.perm)('penalty').can('read'), _api.validateParams], (0, _api.asyncRoute)(async function (req, res) {
+    var result = [];
+    var attendanceUser = await _attendanceUser2.default.find();
+    var penaltyConfig = await _PenaltyConfig2.default.find();
+    if (attendanceUser != null) {
+        attendanceUser.forEach(user => {
+            user.status.forEach(element => {
+                if (element.state == 'late') {
+                    var Val = penaltyConfig.find((item, idx) => {
+                        return item.key === '지각';
+                    });
+                    result.push({
+                        type_id: Val._id,
+                        username: user.name,
+                        type: '지각',
+                        date: moment(element.date).format('YYYY-MM-DD'),
+                        description: '지각',
+                        point: Val.value
+                    });
+                }
+                if (element.state == 'absence') {
+                    var val = penaltyConfig.find((item, idx) => {
+                        return item.key === '결석';
+                    });
+                    result.push({
+                        type_id: val._id,
+                        username: user.name,
+                        type: '결석',
+                        date: moment(element.date).format('YYYY-MM-DD'),
+                        description: '결석',
+                        point: val.value
+                    });
+                }
+            });
+        });
+    }
+    var penalty = await _Penalty2.default.find();
+
+    penalty.forEach(element => {
+        var val = penaltyConfig.find((item, idx) => {
+            return item.key === element.type;
+        });
+        result.push({
+            type_id: val._id,
+            username: element.username,
+            type: element.type,
+            date: moment(element.date).format('YYYY-MM-DD'),
+            description: element.description,
+            point: val.value
+        });
+    });
+
+    res.json(result);
+}));
+
+/**
  * @api {post} /penalty/write/ 상벌점 쓰기
  * @apiDescription 사용자의 상벌점 기록
  * @apiName penaltyWrite
  * @apiGroup Penalty
  * @apiPermission penalty.can.update
  *
- * @apiParam {String} username 조회할 사용자의 아이디
+ * @apiParam {array} users 입력할 사용자들의 아이디 배열
  * @apiParam {String} type 상벌점 항목 타입
  * @apiParam {String} description 상벌점 항목 설명
  * @apiParam {String} date 상벌점 항목 부여 날짜
  *
  * @apiParamExample {post} Request-Example:
  *      {
- *          username : "admin",
+ *          users : {"admin","test01"},
  *          type : "과제 지각",
  *          description : "과제 지각"
  *          date : "2020-03-19"
@@ -148,21 +230,21 @@ router.get('/read/:username', [(0, _role.perm)('penalty').can('read'), (0, _expr
  * @apiSuccessExample {json} Success-Response:
  *      HTTP/1.1 200 OK
  */
-router.post('/write', [(0, _role.perm)('penalty').can('update'), (0, _expressValidator.body)('type_id').isString(), (0, _expressValidator.body)('type').isString(), (0, _expressValidator.body)('date').isString(), (0, _expressValidator.body)('username').isString(), (0, _expressValidator.body)('description').isString(), _api.validateParams], (0, _api.asyncRoute)(async function (req, res) {
-    var penalty = new _Penalty2.default();
-    penalty.type_id = req.body.type_id;
-    penalty.type = req.body.type;
-    penalty.username = req.body.username;
-    penalty.date = req.body.date;
-    penalty.description = req.body.description;
-
-    await penalty.save();
-
+router.post('/write', [(0, _role.perm)('penalty').can('update'), (0, _expressValidator.body)('type_id').isString(), (0, _expressValidator.body)('type').isString(), (0, _expressValidator.body)('date').isString(), (0, _expressValidator.body)('users').isArray(), (0, _expressValidator.body)('description').isString(), _api.validateParams], (0, _api.asyncRoute)(async function (req, res) {
+    req.body.users.forEach(async username => {
+        var penalty = new _Penalty2.default();
+        penalty.type_id = req.body.type_id;
+        penalty.type = req.body.type;
+        penalty.username = username;
+        penalty.date = req.body.date;
+        penalty.description = req.body.description;
+        await penalty.save();
+    });
     res.end();
 }));
 
 /**
- * @api {post} /penalty/delete/ 상벌점 삭제
+ * @api {delete} /penalty/delete 상벌점 삭제
  * @apiDescription 사용자의 상벌점 삭제
  * @apiName penaltyDelete
  * @apiGroup Penalty
@@ -173,7 +255,7 @@ router.post('/write', [(0, _role.perm)('penalty').can('update'), (0, _expressVal
  * @apiParam {String} description 상벌점 항목 설명
  * @apiParam {String} date 상벌점 항목 부여 날짜
  *
- * @apiParamExample {post} Request-Example:
+ * @apiParamExample {delete} Request-Example:
  *      {
  *          username : "admin",
  *          type : "과제 지각",
@@ -186,11 +268,10 @@ router.post('/write', [(0, _role.perm)('penalty').can('update'), (0, _expressVal
  * @apiSuccessExample {json} Success-Response:
  *      HTTP/1.1 200 OK
  */
-router.delete('/:username', [(0, _role.perm)('penalty').can('update'), (0, _expressValidator.param)('username').isString(), (0, _expressValidator.query)('date').isString(), (0, _expressValidator.query)('type').isString(), (0, _expressValidator.query)('description').isString(), (0, _expressValidator.query)('type_id').isString(), _api.validateParams], (0, _api.asyncRoute)(async function (req, res) {
+router.delete('/delete', [(0, _role.perm)('penalty').can('update'), (0, _expressValidator.query)('username').isString(), (0, _expressValidator.query)('date').isString(), (0, _expressValidator.query)('type').isString(), (0, _expressValidator.query)('description').isString(), _api.validateParams], (0, _api.asyncRoute)(async function (req, res) {
     await _Penalty2.default.findOneAndDelete({
-        type_id: req.query.type_id,
         type: req.query.type,
-        username: req.params.username,
+        username: req.query.username,
         date: req.query.date,
         description: req.query.description
     });
--- src/api/v1/penalty.route.js
+++ src/api/v1/penalty.route.js
@@ -113,20 +113,108 @@ router.get(
 )
 
 /**
+ * @api {get} /penalty/read 상벌점 전체 조회
+ * @apiDescription 전체 상벌점 조회
+ * @apiName penaltyReadAll
+ * @apiGroup Penalty
+ * @apiPermission penalty.can.read
+ *
+ *
+ * @apiSuccess {Array} - 사용자의 상벌점 항목을 배열로 반환
+ * @apiSuccess {String} type 상벌점 항목의 타입
+ * @apiSuccess {String} username 상벌점 항목의 사용자
+ * @apiSuccess {String} date 상벌점 항목이 기록된 날짜
+ * @apiSuccess {String} description 상벌점 항목의 설명
+ * @apiSuccess {Number} point 상벌점 항목의 점수
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          [{
+ *              type:"지각",
+ *              username:"admin"
+ *              date:"2020-03-19",
+ *              description:"지각",
+ *              point:-1
+ *          }]
+ *      }
+ */
+router.get(
+    '/read', [
+        perm('penalty').can('read'),
+        validateParams,
+    ],
+    asyncRoute(async function(req, res) {
+        var result = []
+        var attendanceUser = await AttendanceUser.find()
+        var penaltyConfig = await PenaltyConfig.find()
+        if (attendanceUser != null) {
+            attendanceUser.forEach(user=>{
+                user.status.forEach(element=>{
+                        if (element.state == 'late') {
+                            var Val = penaltyConfig.find((item, idx) => {
+                                return item.key === '지각'
+                            })
+                            result.push({
+                                type_id: Val._id,
+                                username: user.name,
+                                type: '지각',
+                                date: moment(element.date).format('YYYY-MM-DD'),
+                                description: '지각',
+                                point: Val.value,
+                            })
+                        }
+                        if (element.state == 'absence') {
+                            var val = penaltyConfig.find((item, idx) => {
+                                return item.key === '결석'
+                            })
+                            result.push({
+                                type_id: val._id,
+                                username: user.name,
+                                type: '결석',
+                                date: moment(element.date).format('YYYY-MM-DD'),
+                                description: '결석',
+                                point: val.value,
+                            })
+                        }
+                })
+            })
+        }
+        var penalty = await Penalty.find()
+
+        penalty.forEach(element => {
+            var val = penaltyConfig.find((item, idx) => {
+                return item.key === element.type
+            })
+            result.push({
+                type_id: val._id,
+                username: element.username,
+                type: element.type,
+                date: moment(element.date).format('YYYY-MM-DD'),
+                description: element.description,
+                point: val.value,
+            })
+        })
+
+        res.json(result)
+    })
+)
+
+/**
  * @api {post} /penalty/write/ 상벌점 쓰기
  * @apiDescription 사용자의 상벌점 기록
  * @apiName penaltyWrite
  * @apiGroup Penalty
  * @apiPermission penalty.can.update
  *
- * @apiParam {String} username 조회할 사용자의 아이디
+ * @apiParam {array} users 입력할 사용자들의 아이디 배열
  * @apiParam {String} type 상벌점 항목 타입
  * @apiParam {String} description 상벌점 항목 설명
  * @apiParam {String} date 상벌점 항목 부여 날짜
  *
  * @apiParamExample {post} Request-Example:
  *      {
- *          username : "admin",
+ *          users : {"admin","test01"},
  *          type : "과제 지각",
  *          description : "과제 지각"
  *          date : "2020-03-19"
@@ -143,26 +231,26 @@ router.post(
         body('type_id').isString(),
         body('type').isString(),
         body('date').isString(),
-        body('username').isString(),
+        body('users').isArray(),
         body('description').isString(),
         validateParams,
     ],
     asyncRoute(async function(req, res) {
-        var penalty = new Penalty()
-        penalty.type_id = req.body.type_id
-        penalty.type = req.body.type
-        penalty.username = req.body.username
-        penalty.date = req.body.date
-        penalty.description = req.body.description
-
-        await penalty.save()
-
+        req.body.users.forEach(async(username)=>{
+            var penalty = new Penalty()
+            penalty.type_id = req.body.type_id
+            penalty.type = req.body.type
+            penalty.username = username
+            penalty.date = req.body.date
+            penalty.description = req.body.description
+            await penalty.save()
+        })
         res.end()
     })
 )
 
 /**
- * @api {post} /penalty/delete/ 상벌점 삭제
+ * @api {delete} /penalty/delete 상벌점 삭제
  * @apiDescription 사용자의 상벌점 삭제
  * @apiName penaltyDelete
  * @apiGroup Penalty
@@ -173,7 +261,7 @@ router.post(
  * @apiParam {String} description 상벌점 항목 설명
  * @apiParam {String} date 상벌점 항목 부여 날짜
  *
- * @apiParamExample {post} Request-Example:
+ * @apiParamExample {delete} Request-Example:
  *      {
  *          username : "admin",
  *          type : "과제 지각",
@@ -187,20 +275,18 @@ router.post(
  *      HTTP/1.1 200 OK
  */
 router.delete(
-    '/:username', [
+    '/delete', [
         perm('penalty').can('update'),
-        param('username').isString(),
+        query('username').isString(),
         query('date').isString(),
         query('type').isString(),
         query('description').isString(),
-        query('type_id').isString(),
         validateParams,
     ],
     asyncRoute(async function(req, res) {
         await Penalty.findOneAndDelete({
-            type_id: req.query.type_id,
             type: req.query.type,
-            username: req.params.username,
+            username: req.query.username,
             date: req.query.date,
             description: req.query.description,
         })
