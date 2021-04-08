e0454d6759df0e0c0f5c7b0b0d7c0f35493e2db5
hschoi1104 <hschoi1104@gmail.com>
2021-04-09 04:32:06 +0900

    [#166] fix - socket 리디렉션 에러 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -51,7 +51,7 @@ store.dispatch('auth/restoreEditToken')
 
 // socket.io
 import io from 'socket.io-client'
-const socket = io('http://www.alcuk.co.kr/socket', { secure: true })
+const socket = io(process.env.socket.url)
 Vue.prototype.$socket = socket
 
 new Vue({
