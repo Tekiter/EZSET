5591d001baf52c7ac07884ba6c539e18cacfb192
hschoi1104 <hschoi1104@gmail.com>
2021-05-04 13:33:51 +0900

    [#187] feat - vue.config.js 수정
    - socket devServer 세팅 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,21 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
+
     publicPath: '/',
+
     outputDir: '../backend/public/',
+
     devServer: {
         proxy: {
             '/api': {
                 target: 'http://localhost:5000',
+
+                changeOrigin: true,
+            },
+
+            '/socket.io': {
+                target: 'http://localhost:5050',
+
                 changeOrigin: true,
             },
         },
